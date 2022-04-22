f05892a34488793dd9705c899667960db4b3956f
Pol Seznec <seznecpol@gmail.com>
2022-04-22 19:08:11 +0200

    TEESSSTTT

--- config/db.js
+++ config/db.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(`mongodb+srv://${process.env.DB_KEY_USERNAME}:${process.env.DB_KEY_PASSWORD}@cluster0.vmk37.mongodb.net/test`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-
-    })
-    .then((self) => console.log(`Connected to "Mongodb/${self.connection.name}"`))
-    .catch((err) => console.log(`Connect to mongoDb failed, error :"${err}"`))
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_KEY_USERNAME}:${process.env.DB_KEY_PASSWORD}@cluster0.vmk37.mongodb.net/test`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then((self) => console.log(`connected to "Mongodb/${self.connection.name}"`))
+  .catch((err) => console.log(`Connect to mongoDb failed, error :"${err}"`));
